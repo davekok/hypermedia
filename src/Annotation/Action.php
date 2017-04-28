f709ea24887e7f673dc32b9395fc962ff6e953d6
Dave Kok <email@davekok.nl>
2017-04-28 13:30:02 +0200

    clearer exception text

--- src/Annotation/Action.php
+++ src/Annotation/Action.php
@@ -76,7 +76,7 @@ final class Action
 				$done.= $matches[0];
 				$text = substr($text, strlen($matches[0]));
 			} else {
-				throw new \LogicException("Syntax error near |$done| |$text|.");
+				throw new \LogicException("Syntax error near $done$text.\n".str_repeat(" ",18+strlen($done))."^\n");
 			}
 		}
 	}
