3c3368532971a187f0efea7398a24cb25489ca85
Rizky Fakkel <rfakkel@deanone.nl>
2018-03-27 11:22:12 +0200

    fix DateType not adding min value

--- src/Meta/Type/DateType.php
+++ src/Meta/Type/DateType.php
@@ -8,6 +8,8 @@ use DateTime,stdClass;
 final class DateType extends Type
 {
 	const type = "date";
+	private $minimumRange;
+	private $maximumRange;
 
 	/**
 	 * Constructor
@@ -16,6 +18,11 @@ final class DateType extends Type
 	 */
 	public function __construct(string $state = null)
 	{
+		if ($state !== null) {
+			[$min, $max] = explode(",", $state);
+			$this->minimumRange = strlen($min) ? $min : null;
+			$this->maximumRange = strlen($max) ? $min : null;
+		}
 	}
 
 	/**
@@ -25,7 +32,7 @@ final class DateType extends Type
 	 */
 	public function getDescriptor(): string
 	{
-		return self::type;
+		return self::type.":".$this->minimumRange.",".$this->maximumRange;
 	}
 
 	/**
@@ -37,6 +44,59 @@ final class DateType extends Type
 	public function meta(stdClass $meta, array $state): void
 	{
 		$meta->type = self::type;
+		if (isset($this->minimumRange)) {
+			if ($this->minimumRange[0] === '$') {
+				$key = substr($this->minimumRange,1);
+				if (isset($state[$key])) {
+					$meta->min = $state[$key];
+				}
+			} else {
+				$meta->min = $this->minimumRange;
+			}
+		}
+
+		if (isset($this->maximumRange)) {
+			if ($this->maximumRange[0] === '$') {
+				$key = substr($this->maximumRange,1);
+				if (isset($state[$key])) {
+					$meta->max = $state[$key];
+				}
+			} else {
+				$meta->max = $this->maximumRange;
+			}
+		}
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getMinimumRange()
+	{
+		return $this->minimumRange;
+	}
+
+	/**
+	 * @param string $minimumRange
+	 */
+	public function setMinimumRange(string $minimumRange)
+	{
+		$this->minimumRange = $minimumRange;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getMaximumRange()
+	{
+		return $this->maximumRange;
+	}
+
+	/**
+	 * @param string $maximumRange
+	 */
+	public function setMaximumRange(string $maximumRange)
+	{
+		$this->maximumRange = $maximumRange;
 	}
 
 	/**
