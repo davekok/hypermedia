66e59ba576bf2286513b627c2272b4f2afd5b76e
Rizky Fakkel <rfakkel@deanone.nl>
2018-04-19 09:30:17 +0200

    PR fixes

--- src/Meta/FieldParser.php
+++ src/Meta/FieldParser.php
@@ -56,7 +56,6 @@ final class FieldParser
 	private $minlengthToken;
 	private $maxlengthToken;
 	private $patternToken;
-	private $orderToken;
 	private $tagToken;
 	private $equalsToken;
 	private $valueToken;
